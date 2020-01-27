0ac55e690bb8c2b625e8f576ee4c8d8038773ebc
fadimata tillman <fadi.tillman@gmail.com>
2020-01-27 20:21:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-create_an_array.unshift("dog")
+  array = [ "I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
