5dc43945caa27855ff064e652c59ddd81ad73cd7
fadimata tillman <fadi.tillman@gmail.com>
2020-01-27 20:18:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,6 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   array.push("arrays!")
-  puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
