e475f20b15a92459eb1171f46da4780450ef139e
fadimata tillman <fadi.tillman@gmail.com>
2020-01-27 19:30:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["fadi","mike","alex","william"]
 end
 
+
 def add_element_to_end_of_array(array, element)
-  
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+retrieve_last_element_from_array[-1]
 end
