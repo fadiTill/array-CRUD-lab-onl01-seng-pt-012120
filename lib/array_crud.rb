7a640eee0736f3d160ccef7ff510756b3602687a
fadimata tillman <fadi.tillman@gmail.com>
2020-01-27 20:29:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,5 +41,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-create_an_array[-1]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array[-1]
 end
